21d131d9c118d824a9a68225132d77ea12d93a31
miguel-l-b <miguellopesbraido@gmail.com>
2023-08-12 15:20:09 -0300

    fix: classe `bluetoothManager`

--- connections.py
+++ connections.py
@@ -1,73 +1,8 @@
-from machine import Timer, Pin
-import network
 import ubluetooth
-from time import sleep, sleep_ms
-
-ble_msg = ""
-
-class Bluetooth():
-    def __init__(self, name):
-        # Create internal objects for the onboard LED
-        # blinking when no BLE device is connected
-        # stable ON when connected
-        self.led = Pin(2, Pin.OUT)
-        self.timer1 = Timer(0)
-        
-        self.name = name
-        self.ble = ubluetooth.BLE()
-        self.ble.active(True)
-        self.disconnected()
-        self.ble.irq(self.ble_irq)
-        self.register()
-        self.advertiser()
-
-    def connected(self):
-        self.led.value(1)
-        self.timer1.deinit()
-
-    def disconnected(self):        
-        self.timer1.init(period=100, mode=Timer.PERIODIC, callback=lambda t: self.led.value(not self.led.value()))
-
-    def ble_irq(self, event, data):
-        global ble_msg
-        
-        if event == 1: #_IRQ_CENTRAL_CONNECT:
-                       # A central has connected to this peripheral
-            self.connected()
-
-        elif event == 2: #_IRQ_CENTRAL_DISCONNECT:
-                         # A central has disconnected from this peripheral.
-            self.advertiser()
-            self.disconnected()
-        
-        elif event == 3: #_IRQ_GATTS_WRITE:
-                         # A client has written to this characteristic or descriptor.          
-            buffer = self.ble.gatts_read(self.rx)
-            ble_msg = buffer.decode('UTF-8').strip()
-            
-    def register(self):        
-        # Nordic UART Service (NUS)
-        NUS_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
-        RX_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
-        TX_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
-            
-        BLE_NUS = ubluetooth.UUID(NUS_UUID)
-        BLE_RX = (ubluetooth.UUID(RX_UUID), ubluetooth.FLAG_WRITE)
-        BLE_TX = (ubluetooth.UUID(TX_UUID), ubluetooth.FLAG_NOTIFY)
-            
-        BLE_UART = (BLE_NUS, (BLE_TX, BLE_RX,))
-        SERVICES = (BLE_UART, )
-        ((self.tx, self.rx,), ) = self.ble.gatts_register_services(SERVICES)
-
-    def send(self, data):
-        self.ble.gatts_notify(0, self.tx, data + 'n')
-
-    def advertiser(self):
-        name = bytes(self.name, 'UTF-8')
-        adv_data = bytearray('x02x01x02') + bytearray((len(name) + 1, 0x09)) + name
-        self.ble.gap_advertise(100, adv_data)
-        print(adv_data)
-        print("rn")
+import network
+from machine import Pin
+from machine import Timer
+from time import sleep
 
 class WiFiManager:
     wifi = network.WLAN(network.STA_IF)
@@ -104,3 +39,72 @@ class WiFiManager:
                 print(".", end="")
                 sleep(1)
         return False
+    
+    @staticmethod
+    def disconnect() -> None:
+        WiFiManager.wifi.disconnect()
+        WiFiManager.wifi.active(False)
+        print("disconnecting wifi")
+class BluetoothManager():
+    def __init__(self, name):
+        self.__led = Pin(13, Pin.OUT)
+        self.__timer1 = Timer(0)
+        self.__ble_msg = ""
+        
+        self.__name = name
+        self.__ble = ubluetooth.BLE()
+        self.__ble.active(True)
+        self.__disconnected()
+        self.__ble.irq(self.__ble_irq)
+        self.__register()
+        self.__advertiser()
+
+    def __connected(self):
+        self.__led.value(0)
+        self.__timer1.deinit()
+
+    def __disconnected(self):        
+        self.__timer1.init(period=100, mode=Timer.PERIODIC, callback=lambda t: self.__led.value(3))
+
+    def __ble_irq(self, event, _data):      
+        if event == 1: #_IRQ_CENTRAL_CONNECT:
+                       # A central has connected to this peripheral
+            self.__connected()
+
+        elif event == 2: #_IRQ_CENTRAL_DISCONNECT:
+                         # A central has disconnected from this peripheral.
+            self.__disconnected()
+            self.__advertiser()
+        
+        elif event == 3: #_IRQ_GATTS_WRITE:
+                         # A client has written to this characteristic or descriptor.          
+            buffer = self.__ble.gatts_read(self.__rx)
+            self.__ble_msg = buffer.decode('UTF-8').strip()
+            
+    def __register(self):        
+        # Nordic UART Service (NUS)
+        NUS_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
+        RX_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
+        TX_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
+            
+        BLE_NUS = ubluetooth.UUID(NUS_UUID)
+        BLE_RX = (ubluetooth.UUID(RX_UUID), ubluetooth.FLAG_WRITE)
+        BLE_TX = (ubluetooth.UUID(TX_UUID), ubluetooth.FLAG_NOTIFY)
+            
+        BLE_UART = (BLE_NUS, (BLE_TX, BLE_RX,))
+        SERVICES = (BLE_UART, )
+        ((self.__tx, self.__rx,), ) = self.__ble.gatts_register_services(SERVICES)
+
+    def send(self, data: str, end="\n") -> None:
+        self.__ble.gatts_notify(0, self.__tx, data + end)
+
+    def write(self) -> str:
+        msg = self.__ble_msg
+        while msg == "": msg = self.__ble_msg
+        self.__ble_msg = ""
+        return msg
+
+    def __advertiser(self):
+        name = bytes(self.__name, 'UTF-8')
+        adv_data = bytearray(b'\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name
+        self.__ble.gap_advertise(100, adv_data)
