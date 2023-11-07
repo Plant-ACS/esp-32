241215bbf8b22cf172dd106846e63b80510c9b8a
Bruno <brunoconcli@gmail.com>
2023-11-07 10:54:42 -0300

    feat: Create new commands related to `sensors`

--- boot.py
+++ boot.py
@@ -2,22 +2,23 @@ import sys
 from time import sleep
 from connections import WiFiManager, BluetoothManager
 from entities import command
-
+from sensors import Relay, Sensor, LDR, Moisture 
 blue = BluetoothManager("ACS #9181")
 
+sensorsConnected = []
 commands = command.Command()
-    
+
 commands.add(
     "list-wifi",
     lambda: list(map(lambda e: blue.send(str(e)), WiFiManager.getList()))
 )
 commands.add(
-        "connect-wifi",
-        lambda: 
-            blue.send("connected to wifi") 
-            if WiFiManager.connect([blue.send("SSID: ", end=""), blue.write()][1], [blue.send("Password: ", end=""), blue.write()][1])
-            else blue.send("failed to connect to wifi")
-    )
+    "connect-wifi",
+    lambda: 
+        blue.send("connected to wifi") 
+        if WiFiManager.connect([blue.send("SSID: ", end=""), blue.write()][1], [blue.send("Password: ", end=""), blue.write()][1])
+        else blue.send("failed to connect to wifi")
+)
 commands.add(
     "disconnect-wifi",
     lambda: [WiFiManager.disconnect(), blue.send("disconnected from wifi")]
@@ -25,6 +26,25 @@ commands.add(
 commands.add(
     "exit",
     lambda: [blue.send("Bye"), sys.exit()]
+)
+commands.add(
+    "add-sensor",
+    lambda: 
+        sensorsConnected.append(LDR(commands.get("create-sensor"))) 
+        if [blue.send("Sensor type (ldr, moisture): "), blue.write()][1] 
+        else sensorsConnected.append(Moisture(commands.get("create-sensor")))
+)
+commands.add(
+    "create-sensor",
+    lambda: 
+        Sensor([blue.send("Port: ", end=""), blue.write()][1], 
+        [blue.send("Minimum value (optional): ", end=""), blue.write()][1]
+        [blue.send("Maximum value (optional): ", end=""), blue.write()][1]
+        )
+)
+commands.add(
+    "list-sensors",
+    lambda: list(map(lambda s: blue.send(str([s.getType(), s.getPort()])), sensorsConnected))
 )
 
 while True:
