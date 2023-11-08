8ab20368b8074bb5287f7dd41abe1341ee96b400
Bruno Concli <brunoconcli@gmail.com>
2023-11-08 17:41:51 -0300

    ref: Adjustments to `Sensor`

--- sensors.py
+++ sensors.py
@@ -14,20 +14,20 @@ class Relay:
       self.setValue(0)
 
 class Sensor:
-   def __init__(self, port: int, min: int = 0, max: int = 10):
+   def __init__(self, port: int, min: int = 0, max: int = 100):
       if min == None: min = 0
-      if max == None: max = 10
+      if max == None: max = 100
 
       if max <= 0:
          raise Exception('Min value is greater or equal to max value')
-      if self.isSensorConnectedIn(port) == False:
-         raise Exception(f"Sensor is not connected in port: {port}")
       if (port == 26 or port == 25):
          raise Exception("Ports 25 and 26 are Digital-to-Analog Converters, choose another port")
+      if self.isSensorConnectedIn(port) == False:
+         raise Exception(f"Sensor is not connected in port: {port}")
       self._min = min
       self._max = max
-      self.pin = port
-      self._pin = ADC(Pin(port))
+      self.port = port
+      self._pin = ADC(Pin(self.port))
       self._pin.width(ADC.WIDTH_10BIT)
 
    @staticmethod
@@ -37,11 +37,12 @@ class Sensor:
       for i in range(100):
          values.append(analog.read())
          sleep(0.05)
-      
+      print(values)
       media = sum(values) / len(values)
+      gap = 60
 
-      for el in values:
-         if el > media +60 or el < media -60:
+      for value in values:
+         if value > media +gap or value < media -gap:
             return False
       return True
    
@@ -55,9 +56,6 @@ class Sensor:
        value = (self.read() / 1023 -1)
        if(value < 0): value *= -1
        return value * (self._max - self._min) + self._min
-   
-   def getPort(self):
-      return self.pin
        
 class LDR(Sensor):
    def __init__(self, port: int = None, min: int = 0, max: int = 10, sensor: Sensor = None):
